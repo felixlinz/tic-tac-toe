ef6dae835fa54f73acc43c707c4b2c598eb7ccba
Felix Linz <felix.linz@icloud.com>
2023-04-25 22:31:30 +0200

    Finally working, no alpha beta pruning yet, new tree every time

--- test_tictactoe.py
+++ test_tictactoe.py
@@ -1,6 +1,8 @@
 from tictactoe import winner
 from tictactoe import utility
 from tictactoe import result
+from tictactoe import player
+from tictactoe import Node
 
 X = "X"
 O = "O"
@@ -34,12 +36,17 @@ board5 = [[EMPTY, EMPTY, EMPTY],
             [EMPTY, O, EMPTY],
             [X, EMPTY, EMPTY]]
 
+board6 = [[X, EMPTY, O],
+          [O, X, O],
+          [X, EMPTY, O]]
+
 def test_winner():
     assert winner(board1) == X
     assert winner(board2) == O
     assert winner(board3) == None
     assert winner(board4) == O
     assert winner(board5) == None
+    assert winner(board6) == O
 
 def test_utility():
     assert utility(board1) == 1
@@ -62,4 +69,13 @@ def test_result():
 
 def test_player():
     assert player(initial_board) == X
-    assert player(board1) == X
+    assert player(board3) == O
+
+def test_depth():
+    d1board = Node(board1)
+    assert d1board.depth() == 6
+    d2board = Node(board2)
+    assert d2board.depth() == 5
+    d3board = Node(board3)
+    assert d3board.depth() == 5
+
--- tictactoe.py
+++ tictactoe.py
@@ -1,9 +1,6 @@
 """
 Tic Tac Toe Player
 """
-"""
-Tic Tac Toe Player
-"""
 import copy
 import math
 
@@ -25,25 +22,14 @@ def player(board):
     """
     Returns player who has the next turn on a board.
     """
-    x = 0
-    o = 0
-    other = 0
-    if terminal(board):
-        return None
+    empty = 0
     for line in board:
         for cell in line:
-            if cell == X:
-                x += 1
-            elif cell == O:
-                o += 1
-            else:
-                other += 1
-    if other == 0:
-        return "papaya"
-    elif x > o:
+            if cell not in [X,O]:
+                empty += 1
+    if (empty % 2) == 0:
         return O
-    else:
-        return X      
+    return X
 
 
 def actions(board):
@@ -56,7 +42,9 @@ def actions(board):
             if cell != X and cell != O:
                 move = (i,e)
                 possible_moves.add(move)
-    return possible_moves
+    if len(possible_moves) != 0:
+        return possible_moves
+    return None
 
 
 def result(board, action):
@@ -64,11 +52,11 @@ def result(board, action):
     Returns the board that results from making move (i, j) on the board.
     """
     try: 
-        fuckaround = copy.deepcopy(board)
-        x,y = action
+        resultboard = copy.deepcopy(board)
+        y,x = action
         operator = player(board)
-        fuckaround[x][y] = operator
-        return fuckaround
+        resultboard[y][x] = operator
+        return resultboard
     except TypeError:
         raise Exception("not a valid move")
 
@@ -97,7 +85,7 @@ def terminal(board):
     """
     Returns True if game is over, False otherwise.
     """
-    if len(list(actions(board))) == 0:
+    if not actions(board):
         return True
     elif winner(board):
         return True
@@ -121,33 +109,29 @@ def minimax(board):
     """
     if len(actions(board)) == 9:
         return (1,1)
-    candidate = Node(board)
-    candidate.value = -100000
-    TicTacTree(candidate)
-    for node in candidate.children:
-        if (value := node.quality()) > candidate.value:
-            candidate = node
-            candidate.value = value
-    return candidate.move
+    elif len(actions(board)) == 1:
+        return (actions(board)).pop()
+    dad = Node(board)
+    candidates = [dad.children[0]]
+    for child in dad.children:
+        child.these_grandchildren()
+        if child.quality() > candidates[-1].quality():
+            candidates.append(child)
+    return candidates[-1].move
 
-
-class TicTacTree:
-    def __init__(self, node):
-        self.now = [node]
-        self.qualitytree = []
-        self.treebuilder()
     
-    def treebuilder(self):
-        while len(self.now) != 0:
-            parent = self.now.pop(0)
-            moves = actions(parent.board)
-            if moves:
-                for move in moves:
-                    node = Node(result(parent.board, move), move)
-                    self.now.append(node)
-                    self.qualitytree.append(node)
-                    parent.children.append(node)
-                
+def treebuilder(node):
+    now = [node]
+    while len(now) > 0:
+        parent = now.pop(0)
+        moves = actions(parent.board)
+        if not terminal(parent.board):
+            for move in moves:
+                moved_board = result(parent.board, move)
+                node = Node(moved_board, move)
+                now.append(node)
+                parent.addchild(node)
+
 
 class Node:
     def __init__(self, board, move = None):
@@ -163,46 +147,56 @@ class Node:
         self.utility = utility(self.board)
         self.target = {X:1, O:-1}
         self.opponent = {X:O, O:X}
-        self.value = -100000
+        if move == None:
+            treebuilder(self)
 
     def addchild(self, node):
         self.children.append(node)
 
-    def quality(self):
-        self.these_grandchildren()
-        intital_value = 0
-        maxdepth = 0
-        mindepth = 100
-        for child, depth in self.losses:
-            if depth > maxdepth:
-                maxdepth = depth
-            elif depth < mindepth:
-                mindepth = depth
-        for child, depth in self.wins:
-            intital_value = intital_value + int(maxdepth/depth)
-        for child, depth in self.losses:
-            intital_value = intital_value - int(math.pow((maxdepth/depth), 2))
-        print("wins", len(self.wins))
-        print("losses", len(self.losses))
-        print("value", intital_value, "depth", mindepth, maxdepth)
-        return intital_value
+    def depth(self):
+        self._depth = 0
+        for row in self.board:
+            for cell in row: 
+                if cell == EMPTY:
+                    self._depth += 1
+        return 9 - self._depth
+
+    def quality(self):  
+        value = 0
+        if self.utility == self.target[self.player]:
+            value += 100000
+        for solution, depth in self.wins:
+            value = value + int(10/depth)*int(10/depth)
+            if depth == 2: 
+                value += 100
+        for solution, depth in self.losses:
+            value = value - int(10/depth)*int(10/depth)
+            if depth == 1: 
+                value = value - 10000
+            elif depth == 3: 
+                value = value - 5000
+        """
+        for loss, depth in self.losses:
+            if depth < 3:
+                win = win - 20000
+        """
+        return len(self.wins) - len(self.losses) + value
     
     def these_grandchildren(self):
         self.allchildren.extend(self.children)
-        depth = 0
+        depth = self.depth()
         while len(self.allchildren) > 0:
-            depth += 1
-            for child in self.allchildren:
-                if child.utility == self.target[self.player]:
-                    self.allchildren.remove(child)
-                    self.wins.append((child, depth))
-                elif child.utility == self.target[self.opponent[self.player]]:
-                    self.allchildren.remove(child)
-                    self.losses.append((child, depth))
-                elif child.terminal == True:
-                    self.allchildren.remove(child)
-                else:
-                    self.allchildren.extend(child.chhildren)
+            child = self.allchildren.pop(0)
+            childdepth = child.depth() - depth 
+            if child.utility == self.target[self.opponent[self.player]]:
+                self.wins.append((child, childdepth))
+                self.grandchildren.append(child)
+            elif child.utility == self.target[self.player]:
+                self.losses.append((child, childdepth))
+                self.grandchildren.append(child)
+            else:
+                self.allchildren.extend(child.children)
+
 
 
                 
